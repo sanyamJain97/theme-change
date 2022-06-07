19a7593af90edc0ee6bfbd42337b13e47e7c3241
sanyamJain97 <sanyamj.jaipur@gmail.com>
2022-06-07 16:12:38 +0530

    Basic toggle theme changes

---
+++ components/ColorModePicker.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="text-center">
+  <h1>Testing color mode</h1>
+    <ul>
+      <li v-for="color of colors" :key="color" @click="colorChange(color)">
+        {{color}}
+      </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ColorModePicker',
+  data() {
+    return {
+      colors: ['dark-mode', 'sepia-mode']
+    }
+  },
+  methods: {
+    colorChange(key) {
+        document.getElementsByTagName('html')[0].className = '';
+        document.getElementsByTagName('html')[0].classList.add(key);
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <ColorModePicker />
+</template>
+
+<script>
+import ColorModePicker from '@/components/ColorModePicker';
+
+export default {
+  name: 'IndexPage',
+  components: {
+    ColorModePicker
+  }
+}
+</script>
